ef6765a0d519af9ef1c18f680e59e795e6ba2727
cptemre <kunduraci2@gmail.com>
2023-02-02 12:12:41 +0100

    Project Upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# ALGORITHM AND DATA STRUCTURES LABORATORY
+
+## TASK 4
+
+### GENERAL INFORMATION
+
+1. This project has been prepared for a course in informatics in the first semester.
+
+2. Project is prepared in Visual Studio 2022 application in Windows 11 OS. It may not work as intended in other platforms.
+
+3. Project only contains the code in github not the project folder prepared by Visual Studio.
+
+### DESCRIPTION OF THE TASK
+
+Present in the diagram the algorithm for calculating the employee's weekly earnings on the basis of
+the number of hours worked.
+The basic salary is the product of the number of hours NH and the hourly wage HW.
+For the number of hours over 40, additional value should be added, assuming
+factor 2.
+As a result, enter the number of hours worked NH and the calculated weekly wage.
+
+For the calculated salary, calculate the annual salary and tax. Options in the program are optional.User decides if he is willing to calculate them
+Earnings up to $ 20,000 are subject to a 19% tax rate.
+Earnings between 20,000 and 30,000 are subject to a tax rate of 21%.
+Earnings over 30,000 are subject to a 28% tax rate.
+As a result, display the annual earnings, tax rate, calculated tax and wages without tax.
---
+++ Task 4.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int fixedH = 40;
+    int factor = 2;
+    int weeklySalary = 0;
+
+    int NH;
+    int HW;
+
+    cout << "Please enter your number of hours.\n";
+    cin >> NH;
+    cout << "Please enter your hourly wage.\n";
+    cin >> HW;
+
+    weeklySalary = fixedH * HW;
+
+    if (NH > fixedH)
+    {
+        weeklySalary += (NH - fixedH) * HW * 2;
+    }
+    
+    int choice;
+
+    cout << "Number of hours: " << NH << "\nHere is your weekly salary: " << weeklySalary << "\nTo continue press 1. To exit press any other number.";
+    cin >> choice;
+
+    int totalTax;
+    int annualSalary;
+    float rate;
+    if (choice == 1)
+    {
+        totalTax = 0;
+        annualSalary = weeklySalary * 52;
+        if (annualSalary <= 20000)
+        {
+            rate = 0.19;
+            totalTax = annualSalary * 0.19;
+        }
+        else {
+            totalTax = 20000 * 0.19;
+            if (annualSalary < 30000)
+            {
+                rate = 0.21;
+                totalTax += (annualSalary - 20000) * 0.21;
+            }
+            else {
+                rate = 0.28;
+                totalTax += 10000 * 0.21 + (annualSalary - 30000) * 0.28;
+            }
+        }
+    }
+
+    cout << "Annual earnings: " << annualSalary << "\nTax rate: " << rate << "\nCalculated tax: " << totalTax << "\nWages without tax: " << annualSalary - totalTax;
+    return 0;
+}
